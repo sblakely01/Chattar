42bd25eb2343fa76927175659cf4eeb26ca99e86
sblakely <stephanye.blakely@gmail.com>
2020-09-15 01:00:56 -0400

    Added react bundle

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-//setTimeout(ReactDOM.render(<App />, document.getElementById('main')), 1500);
+setTimeout(ReactDOM.render(<App />, document.getElementById('main')), 1500);
