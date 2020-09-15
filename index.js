1c3cee27492da5289a08dc4ab216944e3fe281c2
sblakely <stephanye.blakely@gmail.com>
2020-09-15 19:41:07 -0400

    Cors 2

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ const port = process.env.PORT || 3006;
 const path = require('path');
 const bodyParser = require("body-parser");
 const fs = require("fs");
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// Enables CORS
+const cors = require('cors');
+app.use(cors({ origin: true }));
 
 app.use(express.json());
 app.use('/', express.static(path.join(__dirname, '/public')));
